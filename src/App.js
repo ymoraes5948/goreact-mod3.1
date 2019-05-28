1ba3cac1e50301cdbd3fd93612fe386e9263ee32
Yuri <ymoraes5948@gmail.com>
2019-05-28 12:09:51 -0300

    base

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./config/ReactotrionConfig";
+
+import { Provider } from "react-redux";
+
+import store from "./store";
+
+console.tron.log("testando");
+
+const App = () => (
+  <Provider store={store}>
+    <h1>Hello World</h1>
+  </Provider>
+);
+
+export default App;
---
+++ src/store/index.js
@@ -0,0 +1,15 @@
+import { createStore, compose, applyMiddleware } from "redux";
+
+import reducers from "./reducers";
+
+const composer =
+  process.env.NODE_ENV === "development"
+    ? compose(
+        applyMiddleware(...[]),
+        console.tron.createEnhancer()
+      )
+    : applyMiddleware(...[]);
+
+const store = createStore(reducers, composer);
+
+export default store;
