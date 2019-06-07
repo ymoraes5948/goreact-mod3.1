4b60e6b8d0e1959074f7b4a8b6dd349b3e80688e
Yuri <ymoraes5948@gmail.com>
2019-06-07 16:04:49 -0300

    add favorites

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -6,62 +6,66 @@ import { bindActionCreators } from "redux";
 import * as favoriteActions from "../../store/actions/favorites";
 
 class Main extends Component {
-  static propTypes = {
-    addFavorite: PropTypes.func.isRequired,
-    favorites: PropTypes.arrayOf(
-      PropTypes.shape({
-        id: PropTypes.number,
-        name: PropTypes.string,
-        description: PropTypes.string,
-        url: PropTypes.string
-      })
-    ).isRequired
-  };
+    static propTypes = {
+        addFavoriteRequest: PropTypes.func.isRequired,
+        favorites: PropTypes.arrayOf(
+            PropTypes.shape({
+                id: PropTypes.number,
+                name: PropTypes.string,
+                description: PropTypes.string,
+                url: PropTypes.string
+            })
+        ).isRequired
+    };
 
-  state = {
-    repositoryInput: ""
-  };
+    state = {
+        repositoryInput: ""
+    };
 
-  handleAddRepository = event => {
-    event.preventDefault();
+    handleAddRepository = event => {
+        event.preventDefault();
 
-    this.props.addFavorite();
-  };
+        this.props.addFavoriteRequest(this.state.repositoryInput);
+    };
 
-  render() {
-    return (
-      <Fragment>
-        <form onSubmit={this.handleAddRepository}>
-          <input
-            placeholder="usuário/repositório"
-            value={this.state.repositoryInput}
-            onChange={e => this.setState({ repositoryInput: e.target.value })}
-          />
-          <button type="submit">Adicionar</button>
-        </form>
-        <ul>
-          {this.props.favorites.map(favorite => (
-            <li key={favorite.id}>
-              <p>
-                <strong>{favorite.name}</strong>({favorite.description})
-              </p>
-              <a href={favorite.url}>Acessar</a>
-            </li>
-          ))}
-        </ul>
-      </Fragment>
-    );
-  }
+    render() {
+        return ( <
+            Fragment >
+            <
+            form onSubmit = { this.handleAddRepository } >
+            <
+            input placeholder = "usuário/repositório"
+            value = { this.state.repositoryInput }
+            onChange = { e => this.setState({ repositoryInput: e.target.value }) }
+            /> <
+            button type = "submit" > Adicionar < /button> <
+            /form> <
+            ul > {
+                this.props.favorites.map(favorite => ( <
+                    li key = { favorite.id } >
+                    <
+                    p >
+                    <
+                    strong > { favorite.name } < /strong>({favorite.description}) <
+                    /p> <
+                    a href = { favorite.url } > Acessar < /a> <
+                    /li>
+                ))
+            } <
+            /ul> <
+            /Fragment>
+        );
+    }
 }
 
 const mapStateToProps = state => ({
-  favorites: state.favorites
+    favorites: state.favorites
 });
 
 const mapDispatchToProps = dispatch =>
-  bindActionCreators(favoriteActions, dispatch);
+    bindActionCreators(favoriteActions, dispatch);
 
 export default connect(
-  mapStateToProps,
-  mapDispatchToProps
+    mapStateToProps,
+    mapDispatchToProps
 )(Main);
--- src/store/reducers/favorites.js
+++ src/store/reducers/favorites.js
@@ -1,18 +1,10 @@
 const INITIAL_STATE = [];
 
 export default function favorites(state = INITIAL_STATE, action) {
-  switch (action.type) {
-    case "ADD_FAVORITE":
-      return [
-        ...state,
-        {
-          id: Math.random,
-          name: "Facebook/React",
-          description: "a jknijhbcjfkhjiklajlfnds",
-          url: "https://yurimoraes.com.br"
-        }
-      ];
-    default:
-      return state;
-  }
+    switch (action.type) {
+        case "ADD_FAVORITE_SUCCESS":
+            return [...state, action.payload.data];
+        default:
+            return state;
+    }
 }
